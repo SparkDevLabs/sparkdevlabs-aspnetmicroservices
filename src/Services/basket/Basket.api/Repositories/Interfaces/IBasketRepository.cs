3810cb5b797091ad23e7c4d7a9a447c7b9369b3d
Shane Burke <sparkdevlabs@outlook.com>
2021-11-22 13:33:03 -0500

    added the repository and controller work

---
+++ src/Services/basket/Basket.api/Repositories/Interfaces/IBasketRepository.cs
@@ -0,0 +1,12 @@
+﻿using Basket.api.Entities;
+using System.Threading.Tasks;
+
+namespace Basket.api.Repositories.Interfaces
+{
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+        Task DeleteBasket(string userName);
+    }
+}
