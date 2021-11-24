42d3e64544b3631c966a13cb91777bd7939187c9
Shane Burke <sparkdevlabs@outlook.com>
2021-11-24 11:03:54 -0500

    Building out the Order API

---
+++ src/Services/Order/Order.Application/Contracts/Persistence/IOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Order.Domain.Entities;
+
+namespace Order.Application.Contracts.Persistence
+{
+    public interface IOrderRepository : IAsyncRepository<Order.Domain.Entities.Order>
+    {
+        Task<IEnumerable<Order.Domain.Entities.Order>> GetOrdersByUserName(string userName);
+    }
+}
---
+++ src/Services/Order/Order.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Order.Domain.Entities;
+using Order.Application.Features.Orders.Queries.GetOrdersList;
+using Order.Application.Features.Orders.Commands.CheckoutOrder;
+using Order.Application.Features.Orders.Commands.UpdateOrder;
+
+namespace Order.Application.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Order.Domain.Entities.Order, OrdersVM>().ReverseMap();
+            CreateMap<Order.Domain.Entities.Order, CheckoutOrderCommand>().ReverseMap();
+            CreateMap<Order.Domain.Entities.Order, UpdateOrderCommand>().ReverseMap();
+        }
+    }
+}
